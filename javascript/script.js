c77158a4c161932e3fe45d5cf643ea5b417862d6
Antonio <antoniotriolo87@gmail.com>
2017-08-24 02:01:44 +0200

    Loaded all the code

---
+++ javascript/script.js
@@ -0,0 +1,65 @@
+jQuery(document).ready(function () {
+	jQuery.noConflict();
+
+	function filter_images() {
+		var selection = jQuery(".filter_selected").attr("category");
+
+		if (selection == "sweets") {
+			jQuery(".salt").css("display", "none");
+			jQuery(".work_presentation").not(".salt").css("display", "flex");
+		}
+		if (selection == "salts") {
+			jQuery(".salt").css("display", "flex");
+			jQuery(".work_presentation").not(".salt").css("display", "none");
+		}
+		var bLazy = new Blazy({
+			offset: 30000,
+			success: function (element) {}
+		});
+	};
+
+	filter_images();
+
+	jQuery(".button_filter").click(function () {
+		jQuery(".button_filter").toggleClass("filter_selected");
+		filter_images();
+	});
+
+	var bLazy = new Blazy({
+		offset: 30000,
+		success: function (element) {}
+	});
+
+
+
+	jQuery(function () {
+		jQuery(".work_presentation_wrapper").mousewheel(function (event, delta) {
+			this.scrollLeft -= (delta * 1);
+			event.preventDefault();
+			var bLazy = new Blazy({
+				offset: 30000,
+				success: function (element) {}
+			});
+
+		});
+
+	});
+
+	jQuery(".work_presentation").hover(
+		function () {
+			jQuery(this).find(".description").removeClass("display-none");
+		},
+		function () {
+			jQuery(this).find(".description").addClass("display-none");
+		});
+
+		jQuery(".nav_link").click(function(){
+			jQuery(".nav_link").removeClass("nav_link_selected");
+			jQuery(this).addClass("nav_link_selected");
+		});
+	
+	jQuery(".logo_link").click(function(){
+		jQuery(".nav_link").removeClass("nav_link_selected");
+	})
+	
+});
---
+++ send_mail.php
@@ -0,0 +1,16 @@
+<?php
+	$email = $_REQUEST['email'];
+	$name = $_REQUEST['name'];
+	$subject = $_REQUEST['subject'];
+	$message = "MESSAGGIO RICEVUTO DAL PORTFOLIO ---" . $_REQUEST['message'];
+	
+	//settaggi funzionanti per Gmail!!!
+    ini_set("SMTP", "aspmx.l.google.com");
+    ini_set("sendmail_from", "ileniaferdico.do@gmail.com");
+    $headers = "From: $email";
+    mail("ileniaferdico.do@gmail.com", $name . " -- " . $subject, $message, $headers);
+    
+    //redirigo alla home
+    header("Location: index.html");
+    die();
+?>
